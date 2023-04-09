d8dddde5edbda02f6ceb614b6f390053d9cf4bf5
Baoming520 <dxl926@gmail.com>
2023-04-09 21:31:40 +0800

    remove some files which should be ignore in current repository

--- demo/main.py
+++ demo/main.py
@@ -4,9 +4,12 @@ sys.path.append(os.getcwd())
 from just_logger import Logger, LogLevel
 
 if __name__ == '__main__':
-    logger = Logger('demo', LogLevel.INFO)
+    logger = Logger('demo', level=LogLevel.INFO)
     logger.add_stream_handler(LogLevel.INFO)
     logger.add_file_handler('demo.log', LogLevel.INFO)
     logger.log('hello world!', LogLevel.INFO)
+    logger.log('caution please!', LogLevel.WARNING)
     logger.log('bad man!', LogLevel.ERROR)
+    logger.log('fatal error!', LogLevel.CRITICAL)
+    logger.log('debugger!', LogLevel.DEBUG) # Cannot be recorded by level "INFO".
     pass
