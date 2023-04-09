7522042edc69fbdb805fc3478378caa94d530fb3
Baoming520 <dxl926@gmail.com>
2023-04-09 15:27:21 +0800

    initialize just-logger project

---
+++ demo/main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+from just_logger import Logger, LogLevel
+
+if __name__ == '__main__':
+    logger = Logger('demo', LogLevel.INFO)
+    logger.add_stream_handler(LogLevel.INFO)
+    logger.add_file_handler('demo.log', LogLevel.INFO)
+    logger.log('hello world!', LogLevel.INFO)
+    pass
