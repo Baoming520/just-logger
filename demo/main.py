a5bcffb55818fded3d48a349a94763236c5d3fac
Baoming520 <dxl926@gmail.com>
2023-04-16 13:07:55 +0800

    add unit test

--- demo/main.py
+++ demo/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 sys.path.append(os.getcwd())
@@ -7,9 +8,17 @@ if __name__ == '__main__':
     logger = Logger('demo', level=LogLevel.INFO)
     logger.add_stream_handler(LogLevel.INFO)
     logger.add_file_handler('demo.log', LogLevel.INFO)
-    logger.log('hello world!', LogLevel.INFO)
-    logger.log('caution please!', LogLevel.WARNING)
-    logger.log('bad man!', LogLevel.ERROR)
-    logger.log('fatal error!', LogLevel.CRITICAL)
-    logger.log('debugger!', LogLevel.DEBUG) # Cannot be recorded by level "INFO".
+    logger.log('Hello world!', LogLevel.INFO)
+    logger.log('Caution please!', LogLevel.WARNING)
+    logger.log('Error was found!', LogLevel.ERROR)
+    logger.log('Fatal error!', LogLevel.CRITICAL)
+    logger.log('Debugger!', LogLevel.DEBUG) # Cannot be recorded by level "INFO".
+    obj = {
+        'code': 0,
+        'message': 'success',
+        'data': {
+            'content': 'only for testing.'
+        }
+    }
+    logger.log(json.dumps(obj, ensure_ascii=False, indent=4), LogLevel.INFO)
     pass
--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ def readme():
 
 
 setup(
-    name='just_logger', # 项目名称
-    version='0.2.0', # 版本
-    author='Baoming Yu', # 作者
-    author_email='dingxuanliang@icloud.com', # 联系邮箱
-    url='https://github.com/Baoming520/just-logger', # 项目url
-    description='A logging tool is used to record all kinds of logs during program running.', # 简单描述
-    long_description=readme(), # 长描述
-    long_description_content_type="text/markdown", # 长描述格式
-    packages=['just_logger'], # 要打包的package
-    package_data={}, # 要包含的数据文件
-    install_requires=[], # 本库依赖的库
+    name='just_logger', # project name
+    version='0.2.1', # versiion
+    author='Baoming Yu', # author's name
+    author_email='dingxuanliang@icloud.com', # author's email
+    url='https://github.com/Baoming520/just-logger', # url on github
+    description='A logging tool is used to record all kinds of logs during program running.', # abstract
+    long_description=readme(), # contents in README.MD file
+    long_description_content_type="text/markdown", # type of long description
+    packages=['just_logger'], # pakages waiting for packing
+    package_data={}, # data files in package
+    install_requires=[], # required packages
 )
